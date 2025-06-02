df1faf4545f519d0c1baf7c41f7ccc64bcc08ae1
Cihad GUNDOGDU <cihadgundogdu@gmail.com>
2025-06-02 16:13:10 +0300

    Update settings.py to load DEBUG and ALLOWED_HOSTS from environment variables

--- akillisehir/settings.py
+++ akillisehir/settings.py
@@ -9,9 +9,9 @@ load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 INSTALLED_APPS = [
